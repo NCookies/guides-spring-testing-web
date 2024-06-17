d3518347e49b4117456cca73850216d6fdeacdff
NCookies <swstar21c@gmail.com>
2024-06-18 02:20:24 +0900

    init commit

---
+++ src/main/java/site/ncookie/testingweb/GuidesSpringTestingWebApplication.java
@@ -0,0 +1,13 @@
+package site.ncookie.testingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidesSpringTestingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuidesSpringTestingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/ncookie/testingweb/GuidesSpringTestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ncookie.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidesSpringTestingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
