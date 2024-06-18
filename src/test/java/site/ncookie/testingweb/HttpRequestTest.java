24c7c4ab40b90771fecd29eb4c27d67306fd9cb2
NCookies <swstar21c@gmail.com>
2024-06-18 11:05:12 +0900

    HttpRequestTest : 실제 서비스 연결 및 테스트
    - 애플리케이션이 온전한 상태인지 확인하는 것은 좋지만, 애플리케이션의 동작을 확인하는 몇 가지 테스트도 작성해야 한다.
      - 이를 위해서 애플리케이션을 시작하고, 연결을 수신한 다음 (실제 서비스와 마찬가지로) HTTP request와 확인 response를 받을 수 있다.
    - `SpringBootTest.WebEnvironment.RANDOM_PORT`는 서버를 랜덤 포트로 시작하고, `@LocalServerPort`로 포트를 주입한다.
    - `@LocalServerPort`는 테스트 환경에서 포트 충돌을 방지하기 위해 사용한다.
    - 또한 스프링 부트는 자동으로 `TestRestTemplate`를 제공한다. 마찬가지로 `@Autowired`를 사용해 주입된다.

---
+++ src/test/java/site/ncookie/testingweb/HttpRequestTest.java
@@ -0,0 +1,26 @@
+package site.ncookie.testingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    // 테스트 환경에서 포트 충돌을 방지하기 위해 사용
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class))
+                .contains("Hello World");
+    }
+}
